e058580c7a98d86f623fa4dbe116038c1770cd20
Lucas Esteves <70676021+lukesteves92@users.noreply.github.com>
2023-11-19 14:20:56 -0300

    Add files via upload

---
+++ src/screens/Hobbies.js
@@ -0,0 +1,9 @@
+import { Cont } from "../common-components/Container/Container"
+
+export function Hobbies() {
+	return (
+    <Cont>
+      <div>Meu hobbie jogar basquete</div>;
+    </Cont>
+  )
+}
